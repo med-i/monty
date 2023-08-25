eeb623da3f75c2673dca1954ee21922d53aab888
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-25 03:10:55 +0100

    Fix task 2

--- instructions.c
+++ instructions.c
@@ -60,7 +60,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (!*stack)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		clean_resources(1);
+		clean_resources(0);
 		exit(EXIT_FAILURE);
 	}
 
