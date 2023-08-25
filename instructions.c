57223f2d54da91e092bca994e5b028a01acfc3d8
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-25 03:10:20 +0100

    Fix task 2

--- instructions.c
+++ instructions.c
@@ -55,9 +55,6 @@ void pint(stack_t **stack, unsigned int line_number)
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
-
 	stack_t *temp;
 
 	if (!*stack)
